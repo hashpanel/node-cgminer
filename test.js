7732bb16135d288507901f087bd88d7a8c7e0bf1
Travis Webb <me@traviswebb.com>
2014-09-10 02:54:16 -0400

    added tests, renamed connect -> client

---
+++ test.js
@@ -0,0 +1,14 @@
+var assert = require('assert');
+var _ = require('lodash');
+
+describe('cgminer-api', function () {
+
+  describe('@parser', function () {
+
+  });
+
+  describe('@client', function () {
+
+  });
+
+});
